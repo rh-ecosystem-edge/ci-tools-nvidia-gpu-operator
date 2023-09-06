3e1eb805b4a36d3c3dbbf36b78c7728e85bbbbe9
Vitaliy Emporopulo <vemporop@redhat.com>
2023-09-06 13:19:28 +0300

    Point tests against master to latest bundle

--- hack/run_test.sh
+++ hack/run_test.sh
@@ -4,7 +4,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export ARTIFACT_DIR=${ARTIFACT_DIR:="/tmp/gpu-test"}
 export KUBECONFIG=${KUBECONFIG:="$HOME/.kube/config"}
-export GPU_OPERATOR_MASTER_BUNDLE="registry.gitlab.com/nvidia/kubernetes/gpu-operator/staging/gpu-operator-bundle:release-23.03-latest"
+export GPU_OPERATOR_MASTER_BUNDLE="registry.gitlab.com/nvidia/kubernetes/gpu-operator/staging/gpu-operator-bundle:master-latest"
 
 #####################
 ## Setup functions ##
@@ -134,7 +134,7 @@ function clean_artifact_dir() {
     rm -rf "${ARTIFACT_DIR}"/*.xml
     rm -rf "${ARTIFACT_DIR}"/*.version
     rm -rf "${ARTIFACT_DIR}"/*.git_version
-    ls -d "${ARTIFACT_DIR}"/* | grep -P "[0-9]{10}_" | xargs  rm -rf 
+    ls -d "${ARTIFACT_DIR}"/* | grep -P "[0-9]{10}_" | xargs  rm -rf
 }
 
 
